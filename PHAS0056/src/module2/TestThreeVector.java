31af4b20f496709ca029d4b973a1bca573a47b3d
Jan Ciechowski <zcapjpc@ucl.ac.uk>
2018-10-08 16:46:28 +0100

    Added all static and non static versions of vector operations.

--- PHAS0056/src/module2/TestThreeVector.java
+++ PHAS0056/src/module2/TestThreeVector.java
@@ -3,7 +3,6 @@ package module2;
 public class TestThreeVector {
 
 	public static void main(String[] args) {
-		ThreeVector v1 = (4,5,3);
 		
 
 	}
--- PHAS0056/src/module2/ThreeVector.java
+++ PHAS0056/src/module2/ThreeVector.java
@@ -31,7 +31,55 @@ public class ThreeVector {
 		
 		return new ThreeVector(this.x/magVector,this.y/magVector,this.z/magVector); //components are scaled by magnitude
 	}
-	String toString() {
+	
+	//method for listing elements of ThreeVector as string
+	public String toString() {
+		return "x = " + this.x + ", y = " + this.y + ", z = " + this.z;
 		
+	}
+	
+	//all methods below are static cases for the functions required for this exercise:
+	
+	//method for finding scalar product between vectors vec1 and vec2
+	public static double scalarProduct(ThreeVector vec1, ThreeVector vec2) {
+		double dot = vec1.x*vec2.x+vec1.y*vec2.y+vec1.z+vec2.z;
+		return dot;
+	}
+	
+	//method for finding vector products between vectors vec1 and vec2
+	public static ThreeVector vectorProduct(ThreeVector vec1, ThreeVector vec2) {
+		return new ThreeVector(vec1.y*vec2.z-vec1.z*vec2.y,vec1.z*vec2.x-vec1.x*vec2.z,vec1.x*vec2.y-vec1.y*vec2.z);
+		//the above code constructs a new vector as per the standard perscription in 3 dimensions
+	}
+	
+	//method for adding vectors together
+	public static ThreeVector add(ThreeVector vec1, ThreeVector vec2) {
+		return new ThreeVector(vec1.x+vec2.x,vec1.y+vec2.y,vec1.z+vec2.z);
+	}
+	
+	//method for finding the angle between two vectors using methods defined above
+	public static double angle(ThreeVector vec1, ThreeVector vec2) {
+		double ang = Math.acos(scalarProduct(vec1, vec2)/(vec1.magnitude()*vec2.magnitude()));
+		return ang;
+	}
+	
+	//for non-static versions:
+	
+	//scalar product
+	public double scalarProduct(ThreeVector vec) {
+		return scalarProduct(this,vec);
+	}
+	
+	//vector product
+	public ThreeVector vectorProduct(ThreeVector vec) {
+		return vectorProduct(this, vec);
+	}
+	//addition
+	public ThreeVector add(ThreeVector vec) {
+		return add(this,vec);
+	}
+	//angle
+	public double angle(ThreeVector vec) {
+		return angle(this, vec);
 	}
 }
