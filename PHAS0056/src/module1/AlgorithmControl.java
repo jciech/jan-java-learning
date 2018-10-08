517796c271d4ce6197e78d8053aea00f0bdaec0c
Jan Ciechowski <zcapjpc@ucl.ac.uk>
2018-10-08 11:29:55 +0100

    Algorithm control timer with set number of seconds and loop increment version.

--- PHAS0056/src/module1/AlgorithmControl.java
+++ PHAS0056/src/module1/AlgorithmControl.java
@@ -3,8 +3,39 @@ package module1;
 public class AlgorithmControl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	timer();
 	}
-
+	static void loop(){
+		int i = 0;
+		for(i = 1; i <= 8; i++) {
+			System.out.println(i);
+		}
+	}
+	static void decrement() {
+		int i = 10;
+		while (i>=-5) {
+			System.out.println(i);
+			i--;
+		}
+	}
+	static void increment() {
+		double i = 2.5;
+		while (i<=4.3) {
+			System.out.println(i);
+			i = i + 0.1;
+		}
+	}
+	static void timer() {
+		int i = 0;
+		long timeNow = System.currentTimeMillis();
+		long timeEnd = timeNow+5000;
+		while (System.currentTimeMillis() < timeEnd) {
+			i++;
+			int ireminder = i % 100;
+			if (ireminder == 0) {
+				System.out.println(i);
+				
+			}
+		}
+	}
 }
