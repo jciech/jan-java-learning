fc31b9567c0d955786a99308ded3127de99fff22
Jan Ciechowski <zcapjpc@ucl.ac.uk>
2018-10-10 10:56:37 +0100

    Created Complex class and started initial work

---
+++ PHAS0056/src/module2/Complex.java
@@ -0,0 +1,19 @@
+package module2;
+
+public class Complex {
+
+	public static void main(String[] args) {
+		
+	}
+	//defining member variables
+	double re;
+	double im;
+	
+	//constructors
+	public Complex() {}
+	public Complex(double re, double im) {
+		this.re = re;
+		this.im = im;
+	}
+	
+}
