b27824075e5ae1e574bc8a83456986b05533c8b5
Jan Ciechowski <zcapjpc@ucl.ac.uk>
2019-09-04 13:54:36 +0200

    Final commit for module 8

--- PHAS0056/src/module8/ThreadsTimer.java
+++ PHAS0056/src/module8/ThreadsTimer.java
@@ -11,9 +11,16 @@ public class ThreadsTimer {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+		// We initialise the time from which we start counting
 		long start_time = System.currentTimeMillis();
+
+		// And define the necessary initial variables
 		long nPoints = 10000000L;
 		int nThreads = 4;
+
+		// We divide the calculation between 4 threads
+		// each of these will calculate their own estimate of pi for a quarter of all
+		// points
 		ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
 		List<Future<Double>> futures = new ArrayList<Future<Double>>();
 		for (int iThread = 0; iThread < nThreads; ++iThread) {
@@ -21,6 +28,8 @@ public class ThreadsTimer {
 			Future<Double> future = threadPool.submit(task);
 			futures.add(future);
 		}
+		// We take the average of these estimates, which is the same as if we performed
+		// the task for 4 times as many points in each case
 		double sum = 0.0;
 		for (int iThread = 0; iThread < nThreads; ++iThread) {
 			double result = futures.get(iThread).get();
@@ -28,21 +37,31 @@ public class ThreadsTimer {
 		}
 		threadPool.shutdown();
 		double pi = sum / nThreads;
-		System.out.println("The time taken to run the program using 4 threads is: ");
-		System.out.println(System.currentTimeMillis()-start_time);
-		System.out.println("and the value of pi calculated is:"+pi);
 		
+		// The relevant outputs are given
+		System.out.println("The time taken to run the program using 4 threads is: ");
+		System.out.println(System.currentTimeMillis() - start_time);
+		System.out.println("and the value of pi calculated is: " + pi);
+		
+		
+		// And we restart the timer to measure the time it takes to do the same task using one thread
 		start_time = System.currentTimeMillis();
+		
+		// Which we initialise below
 		MonteCarloPiCalculatorTask task = new MonteCarloPiCalculatorTask(nPoints);
 		pi = task.call();
-		
+
+		// Again, we print out the relevant outputs
 		System.out.println("Comparatively, the time it takes to run it using one thread is: ");
-		System.out.println(System.currentTimeMillis()-start_time);
-		
+		System.out.println(System.currentTimeMillis() - start_time);
+		System.out.println("and the value of pi calculated is: "+pi);
+
+		// Observations are printed out below
 		System.out.println("Which takes longer, as expected, due to a smaller available processing power");
-		System.out.println("Note it is not 4x less time as there are diminishing returns related to the usage of multiple threads");
-
-
+		System.out.println(
+				"Note it is not 4x less time as there are diminishing returns related to the usage of multiple threads");
+		System.out.println("The accuracy of the estimates should be the same as the effective number of points considered is the same.");
 		
+
 	}
 }
