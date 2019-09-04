0bd5e29a72d1172d7e47c65a8bae4dff7ab54b4f
Jan Ciechowski <zcapjpc@ucl.ac.uk>
2019-09-04 12:02:02 +0200

    Finishing work on CountdownTask and PrimeNumberTask and their main methods

--- PHAS0056/src/module8/PrimeNumberTask.java
+++ PHAS0056/src/module8/PrimeNumberTask.java
@@ -1,11 +1,46 @@
 package module8;
 
+import java.util.ArrayList;
+
 public class PrimeNumberTask implements Runnable {
+
+	ArrayList<Integer> primeList = new ArrayList<Integer>();
+	
+	// Getter for list of primes
+	public ArrayList<Integer> getPrimeList() {
+		return primeList;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+
+		// Starting with 1 and iterating through all numbers
+		for (int i=1; i<Integer.MAX_VALUE; i+=1) {
+			// Checking and adding to prime list
+			if (PrimeNumberTask.isPrime(i)) {
+				PrimeNumberTask.addToPrimeList(i);
+			}			
+		}
 	}
 
+	public static boolean isPrime(int n) {
+		boolean prime = false;
+		
+		// checking if n is a multiple of 2
+		if(n%2==0) {}
+		// if not, we check odd numbers
+		for (int i=3; i*i<=n; i+=2) {
+			if (n%i==0) {
+				prime = true;
+			}
+		}
+		
+		return prime;
+	}
+	
+	
+	public static void addToPrimeList(int n) {
+		this.primeList.add(n);
+	}
+	
 }
--- PHAS0056/src/module8/ThreadsMain.java
+++ PHAS0056/src/module8/ThreadsMain.java
@@ -2,10 +2,21 @@ package module8;
 
 public class ThreadsMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		CountdownTask cdt = new CountdownTask(10);
-		cdt.run();
+		
+		Thread countdownTask = new Thread(new CountdownTask(10));
+		Thread primeNumberTask = new Thread(new PrimeNumberTask());
+		countdownTask.run();
+		primeNumberTask.run();
+		while(true) {
+			if(countdownTask.isInterrupted()) {
+				primeNumberTask.interrupt();
+			}
+		}
+		
+		
+
 		
 	}
 
